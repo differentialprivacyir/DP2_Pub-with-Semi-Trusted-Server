9cef3d3b050c5a788a6e0052c85d65f605cac395
fatemeh babatabar <ftmw.babatabar@gmail.com>
2024-10-01 23:49:47 +0330

    create first perturbation

---
+++ Client/first_perturbation.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+import random
+
+ 
+class first_perturbation:
+    def __init__(self, epsilon, attributes_domain, client_data):
+        self.epsilon = epsilon
+        self.attributes_domain = attributes_domain
+        self.client_data = client_data
+        self.randomized_data = self.anonymizing()
+    
+     
+    def anonymizing(self):
+        perturbation_attributes = []
+        eps = self.epsilon / len(self.attributes_domain.keys())
+        for i in self.attributes_domain.keys():
+            p = np.exp(eps)/(np.exp(eps)+len(self.attributes_domain.get(i))-1)
+            #q = (1-p)/(len(self.attributes_domain.get(i))-1)
+            perturbation_attributes.append(self.generalized_randomize_response(self.client_data.get(i), self.attributes_domain.get(i), p))
+        return perturbation_attributes
+
+
+    def generalized_randomize_response(self, data, data_domain, p):
+        coin = random.random()
+        if coin <= p :
+            return data
+        else:
+            return random.choice([ele for ele in data_domain if ele != data ])
+           
