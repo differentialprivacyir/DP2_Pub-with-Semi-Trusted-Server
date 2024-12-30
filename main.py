325d8fcf7d1d388848354e7be37af581b70f2328
fatemeh babatabar <ftmw.babatabar@gmail.com>
2024-12-30 17:38:48 +0330

    main

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
+import itertools
 import pandas as pd
+
 from Client.first_perturbation import first_perturbation
-from Server.BayesianNetwork import BayesianNetwork 
-from Server.Clustering import clustering 
+from Server.PrivBayes import PrivBayes
+from Server.clustering import clustering
 from Server.PRAM import PRAM
-from Server.StatisticalAnalyses.TVD import TVD 
+from Experimental_Evzluations.correlation import correlation
+from Experimental_Evzluations.TVD import TVD
+
+
+
 
 def read_dataset():
-        input_domain='Data2-coarse.domain'
-        input_data='Data2-coarse.dat'
-    
+        input_domain='dataset/Data14-coarse.domain'
+        input_data='dataset/Data14-coarse.dat'
+
         fd=open(input_domain,'r')
         head_line=fd.readline()
         readrow=head_line.split(" ")
         att_num=int(readrow[0])
         fd.close()
-    
+
         ##########################################################################################
         #Get the attributes info in the domain file
         multilist = []
         fp=open(input_data,"r")
         fp.readline();  ###just for skip the header line
-        
-    
+
+
         while 1:
             line = fp.readline()
             if not line:
-                break 
-            
+                break
+
             line=line.strip("\n")
             temp=line.split(",")
             temp2 = [str(i)+'a' for i in temp]
@@ -35,27 +41,23 @@ def read_dataset():
 
         colomn = [str(i)+'a' for i in range(att_num)]
         df = pd.DataFrame(multilist, columns = colomn)
-        df.to_csv("dataset.csv", index=False)
         return df
 
-
-
 def attributes_domain():
-        input_domain='Data2-coarse.domain'
+        input_domain='dataset/Data14-coarse.domain'
         fd=open(input_domain,'r')
         head_line=fd.readline()
         readrow=head_line.split(" ")
-    
+
         domain = dict()
         i=0
         while 1:
             line = fd.readline()
             if not line:
                 break
-        
+
             line=line.strip("\n")
             readrow=line.split(" ")
-            #print(readrow)
             start_x=0
             dom = []
             for eachit in readrow:
@@ -68,36 +70,103 @@ def attributes_domain():
         fd.close()
         return domain
 
-
 if __name__ == "__main__":
         dataset = read_dataset()
         domains = attributes_domain()
+        _total_tvd_alpha_3_PRAM = 0
 
-        clients_dataset = []
-        for index, row in dataset.iterrows():
-                clients_dataset.append(row.to_dict())
+        _total_tvd_alpha_4_PRAM = 0
 
-        epsilon = 1
-        # client side
-        # first perturbation
-        clients = []
-        for data in clients_dataset:
+        _total_PRAM_correlation = 0
+
+        total_epsilon = 7
+        epsilon = total_epsilon
+        print("epsilon : "+str(total_epsilon))
+
+        for j in range(20):
+            print("Loop number : "+ str(j+1))
+
+            datasett = dataset.copy(deep=True)
+            clients_dataset = dataset.copy(deep=True)
+            clients_dataset2 = dataset.copy(deep=True)
+
+            ## epsilon for each level is 2 but the total is 4
+            
+
+            clients_dataset_list = []
+            for index, row in clients_dataset.iterrows():
+                    clients_dataset_list.append(row.to_dict())
+
+            clients = []
+            for data in clients_dataset_list:
                 client = first_perturbation(epsilon, domains, data)
                 clients.append(client.randomized_data)
-        fd = pd.DataFrame(clients, columns = dataset.columns.to_list())
-        fd.to_csv("firstPerturbation.csv",index=False)
+            FP = pd.DataFrame(clients, columns = dataset.columns.to_list())
+            
 
-        # server side
-        # generate bayesian network (PrivBayes)
-        bn = BayesianNetwork(fd)
-        clu = clustering(bn.BN, domains)
+            # constructing bayesian network  (k = 2)
+            privBayes = PrivBayes(FP)
 
-        P1 = PRAM(epsilon, clu.clusters, clu.PBC, domains, fd)
-        fp = pd.read_csv("firstPerturbation.csv")
+            # constructing clusters of size 3
+            # len3 = False
+            # while not len3:
+            clu = clustering(privBayes.BN, domains, FP)
+                # len3 = all(len(sublist) < 4 for sublist in clu.clusters)
 
 
-        _tvd = TVD(dataset, fp , domains)
-        _tvd1 = TVD(dataset, P1.randomized_data , domains)
+            # invariant PRAM
+            P = PRAM(epsilon, clu.clusters, clu.PBC, domains, FP)
+            
+            corr1 = correlation(datasett,P.randomized_data)
+            print("correlation PRAM : "+ str(corr1.MAE))
+            _total_PRAM_correlation += corr1.MAE
 
+         
+            #### alpha = 3
+            _tvd_alpha_3_PRAM = 0
+            subsets_of_size_3_2 = list(itertools.combinations(domains.keys(), 3))
+            for subset in subsets_of_size_3_2:
+                _tvd_alpha_3_PRAM += TVD(dataset[list(subset)], P.randomized_data[list(subset)] , domains).tvd
+                
+
+            _tvd_alpha_3_average_PRAM = _tvd_alpha_3_PRAM / len(subsets_of_size_3_2)
+            _total_tvd_alpha_3_PRAM +=_tvd_alpha_3_average_PRAM
+            #######################
+
+
+            #### alpha = 4
+            _tvd_alpha_4_PRAM = 0
+           
+            subsets_of_size_4_2 = list(itertools.combinations(domains.keys(), 4))
+            for subset in subsets_of_size_4_2:
+                _tvd_alpha_4_PRAM += TVD(dataset[list(subset)], P.randomized_data[list(subset)] , domains).tvd
+              
+
+            _tvd_alpha_4_average_PRAM = _tvd_alpha_4_PRAM / len(subsets_of_size_4_2)
+            _total_tvd_alpha_4_PRAM +=_tvd_alpha_4_average_PRAM
+          
+            #######################
+
+
+            print("alpah = 3 PRAM.... total variation distance : "+ str(_tvd_alpha_3_average_PRAM))
+            
+            print("alpah = 4 PRAM.... total variation distance : "+ str(_tvd_alpha_4_average_PRAM))
+            print()
+            
+
+
+        _tvdd_3_PRAM = _total_tvd_alpha_3_PRAM / 20
+       
+
+        _tvdd_4_PRAM = _total_tvd_alpha_4_PRAM / 20
         
+
+        total_correlation_PRAM = _total_PRAM_correlation/20
         
+
+        print("PRAM.... average correlation : "+ str(total_correlation_PRAM))
+       
+        print("alpha = 3 PRAM.... average total variation distance : "+ str(_tvdd_3_PRAM))
+
+        print("alpha = 4 PRAM.... average total variation distance : "+ str(_tvdd_4_PRAM))
+       
