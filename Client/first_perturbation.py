1bc5be670e6870710020fbebbc58efc4acc5c4f2
fatemeh babatabar <ftmw.babatabar@gmail.com>
2024-12-30 17:39:40 +0330

    first perturbation

--- Client/first_perturbation.py
+++ Client/first_perturbation.py
@@ -1,30 +1,26 @@
-
 import numpy as np
-import random
+    
 
- 
 class first_perturbation:
     def __init__(self, epsilon, attributes_domain, client_data):
         self.epsilon = epsilon
         self.attributes_domain = attributes_domain
         self.client_data = client_data
         self.randomized_data = self.anonymizing()
-    
-     
+
+
     def anonymizing(self):
         perturbation_attributes = []
-        eps = self.epsilon / len(self.attributes_domain.keys())
+        eps = self.epsilon/ len(self.attributes_domain.keys())
         for i in self.attributes_domain.keys():
             p = np.exp(eps)/(np.exp(eps)+len(self.attributes_domain.get(i))-1)
-            #q = (1-p)/(len(self.attributes_domain.get(i))-1)
-            perturbation_attributes.append(self.generalized_randomize_response(self.client_data.get(i), self.attributes_domain.get(i), p))
+            q = (1-p)/(len(self.attributes_domain.get(i))-1)
+            indexx = self.attributes_domain[i].index(self.client_data.get(i))
+            perturbation_list = [p if i==indexx else q for i in range(len(self.attributes_domain[i]))]
+            perturbation_attributes.append(self.generalized_randomize_response(self.attributes_domain.get(i), perturbation_list))
         return perturbation_attributes
 
 
-    def generalized_randomize_response(self, data, data_domain, p):
-        coin = random.random()
-        if coin <= p :
-            return data
-        else:
-            return random.choice([ele for ele in data_domain if ele != data ])
-           
+    def generalized_randomize_response(self, data_domain, perturbation_list):
+        random_data = np.random.choice(data_domain , p=perturbation_list)
+        return random_data
